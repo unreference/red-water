10db6794111f39980f777cf55d1e8906af4c791c
unreference <unreference@duck.com>
2024-05-25 05:53:43 -0700

    feat(events): Add server tick event

--- src/main/java/me/unreference/core/Core.java
+++ src/main/java/me/unreference/core/Core.java
@@ -1,9 +1,11 @@
 package me.unreference.core;
 
+import me.unreference.core.events.ServerTickEvent;
 import me.unreference.core.managers.ChatManager;
 import me.unreference.core.managers.CommandManager;
 import me.unreference.core.managers.PlayerManager;
 import me.unreference.core.managers.ScoreboardManager;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +13,10 @@ public final class Core extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
+            Bukkit.getServer().getPluginManager().callEvent(new ServerTickEvent());
+        }, 0L, 1L);
 
         registerManager(new CommandManager());
         registerManager(new PlayerManager());
---
+++ src/main/java/me/unreference/core/events/ServerTickEvent.java
@@ -0,0 +1,27 @@
+package me.unreference.core.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class ServerTickEvent extends Event {
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+    private static Integer ticks = 0;
+
+    public ServerTickEvent() {
+        ticks += 1;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    public Integer getTicks() {
+        return ticks;
+    }
+}
